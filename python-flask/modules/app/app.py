d9b6e684ee27b5ce82d47ecc63cac35c64d61817
kramsey458 <kramsey458@gmail.com>
2018-12-04 18:05:18 -0800

    Initial backend commit. Routes for GET and POST user authentication w/ jwt and bcrypt + storing in a MongoDB

---
+++ python-flask/modules/app/app.py
@@ -0,0 +1,36 @@
+''' flask app with mongo '''
+import os
+import json
+import datetime
+from bson.objectid import ObjectId
+from flask import Flask
+from flask_pymongo import PyMongo
+from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
+from bson.objectid import ObjectId
+
+class JSONEncoder(json.JSONEncoder):
+    ''' extend json-encoder class'''
+
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        if isinstance(o, datetime.datetime):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+
+# create the flask object
+app = Flask(__name__)
+
+# add mongo url to flask config, so that flask_pymongo can use it to make connection
+app.config['MONGO_URI'] = "mongodb://localhost:27017/test"
+app.config['JWT_SECRET_KEY'] = "key.txt"
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
+
+mongo = PyMongo(app)
+flask_bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+# use the modified encoder class to handle ObjectId & datetime object while jsonifying the response.
+app.json_encoder = JSONEncoder
+
