1be51ac5efdbb0ed08611d07e03bb74590cbfa4c
kramsey458 <kramsey458@gmail.com>
2019-02-21 08:34:05 -0800

    nuking submodule

--- python-flask/modules/utils/allowed_filenames.py
+++
@@ -1,4 +0,0 @@
-def allowed_file(filename):
-    ALLOWED_EXTENSIONS = set(['csv'])
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
--- python-flask/modules/utils/init_classifier.py
+++
@@ -1,8 +0,0 @@
-from modules.DataTraining import driver as classfier
-from jsonmerge import merge
-
-
-def classify_data(category, headers, filepaths):
-    ontology = merge(category, headers)
-    classfier.classify(ontology, filepaths)
-
